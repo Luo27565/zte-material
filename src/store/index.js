a73312e083a54f8563a5c96dac414af5d627a836
luo <luo27565@gmail.com>
2023-07-11 15:58:49 +0800

    fix:treeData根据sort排序显示

--- src/store/index.js
+++ src/store/index.js
@@ -36,14 +36,28 @@ const zteStore = defineStore('zte', {
       const flag = [...results]
       results.forEach((_, index) => {
         flag[index].title = results[index].title || results[index].name
+        flag[index].sortBy = flag[index].sort ? new Date(flag[index].sort).getTime() : 0
         results[index].nodes.forEach((_, secondIndex) => {
           flag[index].nodes[secondIndex].title = results[index].nodes[secondIndex].title || results[index].nodes[secondIndex].name
+          flag[index].nodes[secondIndex].sortBy = results[index].nodes[secondIndex].sort ? new Date(results[index].nodes[secondIndex].sort).getTime() : 0
           flag[index].nodes[secondIndex].nodes.forEach((_, thirdIndex) => {
             flag[index].nodes[secondIndex].nodes[thirdIndex].nodes = []
             flag[index].nodes[secondIndex].nodes[thirdIndex].title = results[index].nodes[secondIndex].nodes[thirdIndex].title || results[index].nodes[secondIndex].nodes[thirdIndex].name
+            flag[index].nodes[secondIndex].nodes[thirdIndex].sortBy = results[index].nodes[secondIndex].nodes[thirdIndex].sort ? new Date(results[index].nodes[secondIndex].nodes[thirdIndex].sort).getTime() : 0
           })
         })
       })
+
+      function sortChildrenBySortProperty (arr) {
+        arr.forEach(item => {
+          item.nodes.sort((a, b) => b.sortBy - a.sortBy)
+          if (item.nodes.length > 0) {
+            sortChildrenBySortProperty(item.nodes)
+          }
+        })
+      }
+
+      sortChildrenBySortProperty(flag)
       this.treeData = flag
     }
   }
